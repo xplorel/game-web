339fbb64579def311d5d2ac89e2e0665d229b961
xplorel <58066881+xplorel@users.noreply.github.com>
2026-05-24 13:59:28 +0800

    chore: configure Vite base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  base: '/game-web/',
   plugins: [vue()],
   server: {
     host: '0.0.0.0',
